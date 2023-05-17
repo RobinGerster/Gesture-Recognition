92064b26bfaf6256a53698cefb575d551338763e
Robin Gerster <31821147+RobinGerster@users.noreply.github.com>
2023-05-17 14:02:14 +0200

    removing frame data

--- embedding.py
+++ embedding.py
@@ -28,47 +28,31 @@ def extract_frames(video_path):
         frames.append(frame)
     return frames  # return list of tensors directly
 
-def create_frame_data(load_path, save_path):
+def create_frame_embeddings(load_path, save_path):
     files = os.listdir(load_path)
     video_files = [f for f in files if f.endswith('.mp4')]
-    print("Extracting Frames")
+    print("Extracting Frames and Creating Embeddings")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dinov2_vits14.to(device)
     for video_file in tqdm(video_files):
         video_path = os.path.join(load_path, video_file)
         frames = extract_frames(video_path)
         frames_torch = torch.stack(frames)  # stack frames into a 4D tensor
 
-        name_without_extension = os.path.splitext(video_file)[0]
-        output_filename = f"{name_without_extension}_frames.pt"
-        with open(os.path.join(save_path, output_filename), 'wb') as f:
-            torch.save(frames_torch, f)  # save tensor directly
-
-def create_embeddings(load_path, save_path):
-    files = os.listdir(load_path)
-    pt_files = [f for f in files if f.endswith('.pt')]
-    print("\nCreating embeddings")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dinov2_vits14.to(device)
-    for pt_file in pt_files:
-        pt_path = os.path.join(load_path, pt_file)
-        frames_torch = torch.load(pt_path)  # load tensor directly
-
         embeddings = []
-        for frame_torch in tqdm(frames_torch):
+        for frame_torch in frames_torch:
             frame_torch = frame_torch.to(device).unsqueeze(0)
             with torch.no_grad():
                 embedding = dinov2_vits14(frame_torch)[0]
             embeddings.append(embedding.cpu())
         embeddings_torch = torch.stack(embeddings)  # stack embeddings into a 2D tensor
 
-        name_without_extension = os.path.splitext(pt_file)[0]
+        name_without_extension = os.path.splitext(video_file)[0]
         output_filename = f"{name_without_extension}_embeddings.pt"
         print(embeddings_torch.shape)
         with open(os.path.join(save_path, output_filename), 'wb') as f:
             torch.save(embeddings_torch, f)  # save tensor directly
 
-# Uncomment these lines to run the functions
-#create_frame_data("./data/orig_data/train", "./data/frame_data/train")
-create_frame_data("./data/orig_data/test", "./data/frame_data/test")
-
-#create_embeddings("./data/frame_data/train", "./data/embedding_data/train")
-create_embeddings("./data/frame_data/test", "./data/embedding_data/test")
+# Uncomment these lines to run the function
+#create_frame_embeddings("./data/orig_data/train", "./data/embedding_data/train")
+create_frame_embeddings("./data/orig_data/test", "./data/embedding_data/test")
