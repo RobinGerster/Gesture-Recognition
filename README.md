8b5aba9c6c85e1c6f7645242d639bad688cb7fbc
Robin Gerster <31821147+RobinGerster@users.noreply.github.com>
2023-05-18 11:44:21 +0200

    Frame Change Embeddings

--- README.md
+++ README.md
@@ -4,10 +4,15 @@
 
 Before starting the data generation process, please ensure that you have placed the original data from [this Google Drive link](https://drive.google.com/drive/folders/13KHZpweTE1vRGAMF7wqMDE35kDw40Uym) into the `train` and `test` folders located in the `orig_data` directory.
 
-To generate the data for gesture recognition simply execute the `embeddings.py`. The code follows these steps:
+To generate the data for gesture recognition, execute the `embeddings.py` script. The code follows these steps:
 
-1. **Extract Frames**: We extract frames from the video files. Each frame is resized to 224x224 pixels and converted into a PyTorch tensor with RGB color channels. The frames are normalized based on the ImageNet mean and standard deviation. 
+1. **Extract Frames**: Frames are extracted from the video files. Each frame is resized to 224x224 pixels and converted into a PyTorch tensor with RGB color channels. The frames are normalized based on the ImageNet mean and standard deviation.
 
-2. **Create Embeddings**: We use the `dinov2_vits14` model to create embeddings for the frames. The frames are sent to the device (GPU if available, otherwise CPU), and the model generates an embedding of size `384` for each frame. The embeddings are stored as PyTorch tensors in the embedding_data directory, with each video's embeddings saved in a corresponding images_embeddings.pt file. Each of the files has tensor of shape `(num_frames_in_video, 384)`.
+2. **Create Embeddings**: The `dinov2_vits14` model is used to create embeddings for the frames or the difference between consecutive frames. The frames are sent to the device (GPU if available, otherwise CPU), and the model generates an embedding of size `384` for each frame. 
 
-By following these steps, we prepare the data necessary for gesture recognition.
+   - **Frame Embeddings**: For frame embeddings, each frame's tensor is directly fed to the model.
+   - **Frame Differences Embeddings**: For frame differences embeddings, the difference between consecutive frames is calculated and the resulting tensor is fed to the model. For the first frame, a blank frame (tensor of zeros) is subtracted.
+
+3. **Store Embeddings**: The embeddings are stored as PyTorch tensors in the `frame_embeddings` or `diff_frame_embeddings` directory, depending on the type of embeddings created. Each video's embeddings are saved in a corresponding `images_embeddings.pt` file. Each file contains a tensor of shape `(num_frames_in_video, 384)`.
+
+By following these steps, we prepare the data necessary for gesture recognition, either using direct frame embeddings or embeddings of frame differences.
--- embedding.py
+++ embedding.py
@@ -26,32 +26,51 @@ def extract_frames(video_path):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         frame = transform(image)
         frames.append(frame)
+    vidcap.release()
     return frames  # return list of tensors directly
 
-def create_frame_embeddings(load_path, save_path):
+
+def create_frame_embeddings(load_path, save_path, difference=False):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
     files = os.listdir(load_path)
     video_files = [f for f in files if f.endswith('.mp4')]
-    print("Extracting Frames and Creating Embeddings")
+    print("Extracting Frames and Creating Embeddings\n")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dinov2_vits14.to(device)
     for video_file in tqdm(video_files):
         video_path = os.path.join(load_path, video_file)
-        frames = extract_frames(video_path)
-        frames_torch = torch.stack(frames)  # stack frames into a 4D tensor
+        try:
+            frames = extract_frames(video_path)
+        except Exception as e:
+            print(f"Error processing {video_file}: {e}")
+            continue
+
+        frames_torch = torch.stack(frames)
+
+        if difference:
+            blank_frame = torch.zeros_like(frames_torch[0])  # Create a blank frame
+            frames_to_embed = [frames_torch[i] - frames_torch[i - 1] if i > 0 else frames_torch[0] - blank_frame for i
+                               in range(len(frames_torch))]
+            frames_to_embed = torch.stack(frames_to_embed)
+        else:
+            frames_to_embed = frames_torch
 
         embeddings = []
-        for frame_torch in frames_torch:
-            frame_torch = frame_torch.to(device).unsqueeze(0)
+        for frame_to_embed in frames_to_embed:
+            frame_to_embed = frame_to_embed.to(device).unsqueeze(0)
             with torch.no_grad():
-                embedding = dinov2_vits14(frame_torch)[0]
+                embedding = dinov2_vits14(frame_to_embed)[0]
             embeddings.append(embedding.cpu())
-        embeddings_torch = torch.stack(embeddings)  # stack embeddings into a 2D tensor
+        embeddings_torch = torch.stack(embeddings)
 
         name_without_extension = os.path.splitext(video_file)[0]
         output_filename = f"{name_without_extension}_embeddings.pt"
         with open(os.path.join(save_path, output_filename), 'wb') as f:
-            torch.save(embeddings_torch, f)  # save tensor directly
+            torch.save(embeddings_torch, f)
 
 
-create_frame_embeddings("./data/orig_data/train", "data/frame_embeddings/train")
+#create_frame_embeddings("./data/orig_data/train", "data/frame_embeddings/train")
 create_frame_embeddings("./data/orig_data/test", "data/frame_embeddings/test")
+create_frame_embeddings("./data/orig_data/test","data/frame_change_embeddings/test", difference=True)
